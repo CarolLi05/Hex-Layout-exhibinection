beaa8eb59ce7d892bb5c4e8aebafc5630bcf5348
Carol <dinosaur0514@gmail.com>
2021-09-05 23:33:12 +0800

    add & finish product

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,18 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+let swiper = new Swiper(".guess-like-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  breakpoints: {
+    768:{
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  }
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true
+  // },
+  // autoplay: {
+  //   delay: 3000,
+  //   disableOnInteraction: false
+  // }
 });
