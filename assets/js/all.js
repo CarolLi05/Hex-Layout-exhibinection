53227c4afe5db51035f66389639039284afd7675
Carol <dinosaur0514@gmail.com>
2021-09-05 23:32:46 +0800

    Update 2021-09-05T15:32:41.922Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,21 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+var swiper = new Swiper(".guess-like-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  } // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true
+  // },
+  // autoplay: {
+  //   delay: 3000,
+  //   disableOnInteraction: false
+  // }
+
 });
 //# sourceMappingURL=all.js.map
